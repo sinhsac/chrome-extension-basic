cea0c6e0b9cd1e24dd83877aff80600fac28818d
Sac <sac.le@primedata.ai>
2022-07-06 15:24:28 +0700

    using background and update rule

--- src/background.js
+++ src/background.js
@@ -2,3 +2,10 @@
 chrome.action.onClicked.addListener(tab => {
     console.log(tab)
 });
+
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+}, { url: [ { urlMatches: 'https://*/*' } ] })
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+    console.info(details.url, details);
+}, { url: [ { urlMatches: 'https://*/*' } ] });
